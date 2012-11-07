209b4522726756ece19a9369533e4ac6474f32a3
wavesoft <johnys2@gmail.com>
2012-11-07 02:28:48 +0100

    Fixed some typos in contextiso.h

--- include/contextiso.h
+++ include/contextiso.h
@@ -2,18 +2,21 @@
 #ifndef CONTEXTISO_H
 #define CONTEXTISO_H
 
+// For sizeof()
+#include <string.h>
+
 /**
  * Disk size is enough to fit 2KiB of data
  */
 static const int CONTEXTISO_CDROM_SIZE = 358400;
 
 /**
- * Create a cd-rom image that will contain the specified filename with the specified content
+ * Create a CD-ROM image that will contain the specified filename with the specified content
  *
  * @param volume        The name of the volume (CD-ROM Label)
  * @param filename      The name fo the file to put in the CD-ROM
- * @param buffer        The buffer to copy the data from (Maximum 2024 bytes)
- * @param size          The size of the buffer
+ * @param contents      The buffer to copy the file contents from (Maximum 2048 bytes)
+ * @param szContents    The size of the buffer
  *
  * @return              Returns the contents of the CD-ROM buffer. The length of the buffer is CONTEXTISO_CDROM_SIZE
  */
@@ -26,4 +29,4 @@ char * build_simple_cdrom( const char * volume, const char * filename, const cha
  */
 char * build_context_cdrom( const char * user_data ) { return build_simple_cdrom("CONFIGDISK", "CONTEXT.SH", user_data, strlen(user_data)); };
 
-#endif /* end of include guard: CONTEXTISO_H */
+#endif
